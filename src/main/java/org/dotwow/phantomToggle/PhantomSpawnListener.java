02b9f55dda7c7efbd0ddad24c941a9a31d528877
PakshP <pakshpatel1@gmail.com>
2024-08-23 10:50:43 -0400

    completed plugin

---
+++ src/main/java/org/dotwow/phantomToggle/PhantomSpawnListener.java
@@ -0,0 +1,26 @@
+package org.dotwow.phantomToggle;
+
+import org.bukkit.entity.Phantom;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTargetEvent;
+
+public class PhantomSpawnListener implements Listener {
+
+    private final PhantomManager phantomManager;
+
+    public PhantomSpawnListener(PhantomManager phantomManager) {
+        this.phantomManager = phantomManager;
+    }
+
+    @EventHandler
+    public void onPhantomTarget(EntityTargetEvent event) {
+        if (event.getTarget() instanceof Player && event.getEntity() instanceof Phantom) {
+            Player player = (Player) event.getTarget();
+            if (phantomManager.isPhantomSpawningDisabled(player)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
